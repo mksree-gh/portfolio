4ff0bc5f0369e511bb98dad2e9492ff10ca8e720
Keerthi Sree Marrapu <mksree@MacBook-Air.local>
2025-07-22 11:28:24 +0530

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HXZ1YQY655"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HXZ1YQY655');
+            `,
+          }}
+        />
+      </head>
       <body className={jost.className}>
         <ThemeProvider
           attribute="class"
